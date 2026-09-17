e48c57eaa2abde3c0a158d2a346e70db0bb7c3b0
Abhishek Shelar <abhishekshelar2003@gmail.com>
2026-09-18 00:28:12 +0530

    Fix Preview Scene

--- src/pages/Source Control/ObsSourceToggler.jsx
+++ src/pages/Source Control/ObsSourceToggler.jsx
@@ -101,8 +101,8 @@ export default function ObsSourceToggler() {
   const handleVerseView = async () => {
     if (!connected) return;
     try {
-      await obs.call('SetCurrentPreviewScene', { sceneName: 'Verse View' });
-      await obs.call('SetCurrentProgramScene', { sceneName: 'Song Verse TV' });
+      await obs.call('SetCurrentPreviewScene', { sceneName: 'Song Verse TV' });
+      await obs.call('SetCurrentProgramScene', { sceneName: 'Verse View' });
     } catch (error) {
       console.error('Error switching to Verse:', error);
     }
